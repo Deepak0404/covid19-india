ebda7fd5d2b3e4cc00990f9495a8046946ee12ed
Deepak0404 <deepaksaini.net@gmail.com>
2020-06-30 22:27:47 +0530

    Updates

---
+++ precache-manifest.e45bc2a93738f80b1cacedb5f773f816.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66c44e08217729f55df21850586b666c",
+    "url": "/covid19-india/index.html"
+  },
+  {
+    "revision": "090a95438ad0db02bd7e",
+    "url": "/covid19-india/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "69fba2cc3065d5de8c0b",
+    "url": "/covid19-india/static/js/2.f149fc70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-india/static/js/2.f149fc70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "090a95438ad0db02bd7e",
+    "url": "/covid19-india/static/js/main.4799b054.chunk.js"
+  },
+  {
+    "revision": "22a7c1c38bdcdb29c582",
+    "url": "/covid19-india/static/js/runtime-main.ece126ce.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19-india/static/media/logo.5d5d9eef.svg"
+  }
+]);
